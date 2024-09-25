a00076a274bb9d4ca9de50375ac0bdbda4171834
Ids Osinga <ip.osinga@alfa-college.nl>
2024-09-25 12:27:26 +0200

    v2024-09-25

--- eenhedenomrekenen/eenheden.js
+++ eenhedenomrekenen/eenheden.js
@@ -1,9 +1,25 @@
-function hallo(naam)
-{
+const eenheden = ["mm", "cm", "dm", "m", "dam", "hm", "km"];
+
+let dimensie = document.getElementById("dimselect");
+let opgave = document.getElementById("opginput");
+let opgeenheid = document.getElementById("opgeenheid");
+let antwoord = document.getElementById("antwinput");
+let antweenheid = document.getElementById("antweenheid");
+
+function nieuw() {
+    let dimvalue = dimensie.value;
+    if (dimvalue == 0) {
+        alert("Kies eerst een dimensie!")
+    } else {
+        let randomnmbr = Math.floor(Math.random() * 7)
+        //console.log("randomnmbr = " + randomnmbr);
+        //let opgeenh = eenheden[randomnmbr];
+        //console.log(opgeenh)
+        opgeenheid.innerHTML = eenheden[randomnmbr] + "<sup>" + dimvalue + "</sup>";
+    }
+}
+
+function check() {
     alert("Hallo " + naam);
 }
 
-function helloworld()
-{
-    alert("Hello World");
-}
