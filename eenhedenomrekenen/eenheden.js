4427c4b57ae0b4cb1c37ea8c10acc13f64bd3562
Ids Osinga <ip.osinga@alfa-college.nl>
2024-09-30 12:11:58 +0200

    v2024-09-30

--- eenhedenomrekenen/eenheden.js
+++ eenhedenomrekenen/eenheden.js
@@ -1,5 +1,6 @@
 const eenheden = ["mm", "cm", "dm", "m", "dam", "hm", "km"];
-
+//index             0     1     2    3     4      5     6
+//eenheden[3] = "m"
 let dimensie = document.getElementById("dimselect");
 let opgave = document.getElementById("opginput");
 let opgeenheid = document.getElementById("opgeenheid");
@@ -7,16 +8,20 @@ let antwoord = document.getElementById("antwinput");
 let antweenheid = document.getElementById("antweenheid");
 
 function nieuw() {
+    antwoord.value = "";
     let dimvalue = dimensie.value;
-    if (dimvalue == 0) {
-        alert("Kies eerst een dimensie!")
+    let randomnmbr = Math.floor(Math.random() * 7)
+    let randomnmbr2 = Math.floor(Math.random() * 7)
+    if (dimvalue == 1) {
+        opgeenheid.innerHTML = eenheden[randomnmbr];
+        antweenheid.innerHTML = eenheden[randomnmbr2]
     } else {
-        let randomnmbr = Math.floor(Math.random() * 7)
-        //console.log("randomnmbr = " + randomnmbr);
-        //let opgeenh = eenheden[randomnmbr];
-        //console.log(opgeenh)
         opgeenheid.innerHTML = eenheden[randomnmbr] + "<sup>" + dimvalue + "</sup>";
+        antweenheid.innerHTML = eenheden[randomnmbr2] + "<sup>" + dimvalue + "</sup>";
     }
+    //nu nog een willekeurig getal in opgave
+    let opgdec = (Math.random() * 1000).toFixed(3);
+    opgave.value = opgdec;
 }
 
 function check() {
