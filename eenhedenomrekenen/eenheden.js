93ec4dc52a71a6dfdb6fa0fd602ff82ba58f3fcf
Ids Osinga <ip.osinga@alfa-college.nl>
2024-10-02 12:19:48 +0200

    v2024-10-02

--- eenhedenomrekenen/eenheden.js
+++ eenhedenomrekenen/eenheden.js
@@ -22,6 +22,25 @@ function nieuw() {
     //nu nog een willekeurig getal in opgave
     let opgdec = (Math.random() * 1000).toFixed(3);
     opgave.value = opgdec;
+
+    let factor = Math.pow(10, dimvalue);
+    console.log("factor = " + factor);
+    let verschil;
+    // if(randomnmbr > randomnmbr2){
+    //     verschil = randomnmbr - randomnmbr2;
+    // } else {
+    //     verschil = randomnmbr2 - randomnmbr;
+    // }
+    verschil = Math.abs(randomnmbr - randomnmbr2)
+    let factortotaal = Math.pow(factor, verschil);
+    console.log("factortotaal = " + factortotaal);
+    let berekendeantw;
+    if(randomnmbr < randomnmbr2) {
+        berekendeantw = opgdec / factortotaal;
+    } else {
+        berekendeantw = opgdec * factortotaal;
+    }
+    console.log("het juiste antwoord moet zijn: " + berekendeantw);
 }
 
 function check() {
