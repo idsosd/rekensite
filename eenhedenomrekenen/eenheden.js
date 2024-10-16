ae3bb19d350a6c99ad17fcc4485568dd290b264f
Ids Osinga <ip.osinga@alfa-college.nl>
2024-10-16 12:09:07 +0200

    v2024-10-16

--- eenhedenomrekenen/eenheden.js
+++ eenhedenomrekenen/eenheden.js
@@ -7,13 +7,15 @@ let opgeenheid = document.getElementById("opgeenheid");
 let antwoord = document.getElementById("antwinput");
 let antweenheid = document.getElementById("antweenheid");
 
-let berekendeantw;
+let hist = document.getElementById("history");
+
+let berekendeantw = new Decimal(0.12345);
 
 function nieuw() {
     antwoord.value = "";
     let dimvalue = dimensie.value;
-    let randomnmbr = Math.floor(Math.random() * 7)
-    let randomnmbr2 = Math.floor(Math.random() * 7)
+    let randomnmbr = Math.floor(Math.random() * 7);
+    let randomnmbr2 = Math.floor(Math.random() * 7);
     if (dimvalue == 1) {
         opgeenheid.innerHTML = eenheden[randomnmbr];
         antweenheid.innerHTML = eenheden[randomnmbr2]
@@ -22,33 +24,35 @@ function nieuw() {
         antweenheid.innerHTML = eenheden[randomnmbr2] + "<sup>" + dimvalue + "</sup>";
     }
     //nu nog een willekeurig getal in opgave
-    let opgdec = (Math.random() * 1000).toFixed(3);
+    let opgdec = new Decimal((Decimal.random() * 1000).toFixed(3));
     opgave.value = opgdec;
 
-    let factor = Math.pow(10, dimvalue);
+    let factor = new Decimal(Decimal.pow(10, dimvalue));
     console.log("factor = " + factor);
     let verschil;
-    // if(randomnmbr > randomnmbr2){
-    //     verschil = randomnmbr - randomnmbr2;
-    // } else {
-    //     verschil = randomnmbr2 - randomnmbr;
-    // }
-    verschil = Math.abs(randomnmbr - randomnmbr2)
-    let factortotaal = Math.pow(factor, verschil);
+
+    verschil = Decimal.abs(randomnmbr - randomnmbr2);
+    let factortotaal = new Decimal(Decimal.pow(factor, verschil));
     console.log("factortotaal = " + factortotaal);
     
     if(randomnmbr < randomnmbr2) {
-        berekendeantw = opgdec / factortotaal;
+        berekendeantw = Decimal.div(opgdec, factortotaal);
     } else {
-        berekendeantw = opgdec * factortotaal;
+        berekendeantw = Decimal.mul(opgdec, factortotaal);
     }
     console.log("het juiste antwoord moet zijn: " + berekendeantw);
 }
 
 function check() {
-    if(berekendeantw == antwoord.value)
-        alert("GOED!")
+    //nodig: opgave.value, die moet naar hist.innerHTML
+    hist.innerHTML += opgave.value;
+    hist.innerHTML += " " + opgeenheid.innerText; //hier moet opgeenheid komen
+    hist.innerHTML += " = ";
+    hist.innerHTML += antwoord.value;
+    hist.innerHTML += " " + antweenheid.innerText; //hier moet antweenheid komen
+    if(berekendeantw.equals(antwoord.value))
+        hist.innerHTML += " GOED<br>";
     else
-        alert("FOUT!")
+        hist.innerHTML += " FOUT<br>";
 }
 
