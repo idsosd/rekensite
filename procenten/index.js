721b60c99b134edddd3fe7b29218b5b099e28a1c
Ids Osinga <ip.osinga@alfa-college.nl>
2024-12-09 11:14:05 +0100

    v2024-12-06

---
+++ procenten/index.js
@@ -0,0 +1,10 @@
+let oud = document.getElementById("input_oud");
+let type = document.getElementById("select_type");
+let perc = document.getElementById("input_perc");
+let factor = document.getElementById("input_factor");
+let nieuw = document.getElementById("input_nieuw");
+let deler = document.getElementById("input_deler");
+
+function losop(){
+    alert("de knop werkt");
+}
